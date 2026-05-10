5a4218b1dab50eb88bdbe29b8a2f0169f5323fb6
Major Hayden <major@mhtx.net>
2026-05-10 12:52:23 -0500

    feat: add neovim LazyVim configuration
    Signed-off-by: Major Hayden <major@mhtx.net>

---
+++ dot_config/nvim/lua/plugins/projects.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        sources = {
+          projects = {
+            dev = { "~/git", "~/git/major", "~/git/redhat", "~/git/fedora" },
+          },
+        },
+      },
+    },
+  },
+}
