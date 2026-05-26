ab443ea7c0b292e182acbf29116719ed5ed9bd10
Major Hayden <major@mhtx.net>
2026-05-26 09:44:16 -0500

    feat(nvim): force wl-clipboard provider on Wayland
    Signed-off-by: Major Hayden <major@mhtx.net>

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,17 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Force wl-clipboard on Wayland (prevents Neovim from picking xsel)
+vim.g.clipboard = {
+  name = "wl-clipboard",
+  copy = {
+    ["+"] = "wl-copy",
+    ["*"] = "wl-copy",
+  },
+  paste = {
+    ["+"] = "wl-paste",
+    ["*"] = "wl-paste",
+  },
+  cache_enabled = false,
+}
