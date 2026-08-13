1cfa1b700c4863a7f16c9e3802f9902ddeeb3498
Major Hayden <major@mhtx.net>
2026-08-13 07:42:50 -0500

    feat(nvim): stop auto-opening Snacks explorer, show hidden files
    - remove Snacks.explorer() call from autocmds
    
    - configure picker and explorer sources to include hidden/ignored files
    
    Signed-off-by: Major Hayden <major@mhtx.net>

---
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "folke/snacks.nvim",
+    lazy = false,
+    opts = {
+      picker = {
+        sources = {
+          files = {
+            hidden = true,
+            ignored = true,
+          },
+          explorer = {
+            hidden = true,
+            ignored = true,
+          },
+        },
+      },
+    },
+  },
+}
